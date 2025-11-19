092b7fabab43e8f402f92d7e0a59d5505479734e
Alejandro Jaramillo <alejandrojaramillo@Alejandros-MacBook-Air.local>
2025-11-19 18:01:57 +0100

    feat: update homepage and tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
-import Image from "next/image";
-
 export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind funcionando 🚀
+      </h1>
+    </main>
+  );
+}
+
+/*export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -13,7 +21,9 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="text-4xl font-bold text-red-500">Tailwind funcionando 🚀</h1>
+
+          --<h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
@@ -63,3 +73,4 @@ export default function Home() {
     </div>
   );
 }
+*/
